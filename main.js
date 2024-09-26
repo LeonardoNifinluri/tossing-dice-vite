a903673c7478fc1f6fffcc0c0ce88f121282d2e4
Leo <leonardonifinluri@gmail.com>
2024-09-26 11:51:04 +0800

    This is the beggining when using vite

---
+++ main.js
@@ -0,0 +1,36 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+//This is to import jquery
+import $ from 'jquery'
+
+$("#app").html(`
+    <div>
+        <div class="card">
+            <h2 id="headerText" class="cardText"></h2>
+            <button id="changeTextBtn" class="myBtn"></button>
+        </div>
+    </div>
+`)
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
